963a78d3eb57793e655c15d827f231abd611b3dc
Anonymous_N <nam348tnh.3gp@gmail.com>
2026-08-17 06:20:30 +0700

    Create lib/widgets/code_editor.dart

---
+++ app/lib/widgets/code_editor.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_code_editor/flutter_code_editor.dart';
+import 'package:highlight/languages/go.dart';
+
+class CodeEditor extends StatelessWidget {
+  final String code;
+  final ValueChanged<String> onChanged;
+
+  const CodeEditor({required this.code, required this.onChanged, Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = CodeController(
+      text: code,
+      language: go,
+    );
+    controller.addListener(() {
+      onChanged(controller.text);
+    });
+
+    return Container(
+      margin: EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey.shade700),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: CodeField(
+        controller: controller,
+        wrap: true,
+        textStyle: TextStyle(fontFamily: 'monospace', fontSize: 14),
+        lineNumberStyle: LineNumberStyle(
+          margin: 8,
+          textStyle: TextStyle(color: Colors.grey),
+        ),
+      ),
+    );
+  }
+}
